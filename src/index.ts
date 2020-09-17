bd03b6c9cb9109ce15e6a6be6411adf5c6a9f003
akosfi <fiakos1997@gmail.com>
2020-09-17 20:25:19 +0200

    added cors

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,20 @@ require('dotenv').config();
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from 'cookie-parser';
+import cors, {CorsOptions} from 'cors';
 
 import apiRouter from "./routes";
 import {initDB} from "./db";
-import {signJWT, TokenType} from "./utils/JWTUtils";
+
+const corsOptions: CorsOptions= {
+   origin: process.env.CLIENT_APP
+}
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors(corsOptions))
 app.use('/api', apiRouter);
 app.get('/health-check', (req, res, next) => res.send("Hey!"));
 
