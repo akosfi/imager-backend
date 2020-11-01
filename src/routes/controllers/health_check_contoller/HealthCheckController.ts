56f48e6e81288d6363383e6e5cddeed44e7e96db
Akos Fi <fi.akos@straxus.hu>
2020-11-01 22:16:30 +0100

    try feature update

--- src/routes/controllers/health_check_contoller/HealthCheckController.ts
+++ src/routes/controllers/health_check_contoller/HealthCheckController.ts
@@ -3,10 +3,14 @@ const healthCheckRouter = Router();
 
 export enum RouteConfig {
     ROOT = "/",
+    TEST = "/test",
 }
 
 const getServerHealth = (req: Request, res: Response) => res.status(201).send({"status": "Hey!"});
 
+const getNewTestMessage = (req: Request, res: Response) => res.status(201).send({"message": "Hey my man!"});
+
 healthCheckRouter.use(RouteConfig.ROOT, getServerHealth);
+healthCheckRouter.use(RouteConfig.TEST, getNewTestMessage);
 
 export default healthCheckRouter;
